01831ecd1f9f1807a22410f1097f55986ad1354c
GabrielParka <onwujiuba.uzochukwu@gmail.com>
2022-10-13 05:36:59 -0700

    checkmate

---
+++ 0x00-hello_world/parka.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ *
+ * *main - printing is fun
+ *
+ * *
+ *
+ * *Return: Always 0 (Beautiful)
+ *
+ **/
+
+int main(void)
+
+{
+	puts(*\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
