a6ae24e364cb1fe175b6c9d9e56c787dee84b3b5
GabrielParka <onwujiuba.uzochukwu@gmail.com>
2022-10-13 14:43:20 -0700

    ,2-betty commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - entry point for the program.
+  *
+  *Return: by default returns zero if no errors encountered.
+*/
+
+int main(void)
+{
+	puts("\"Programming is like  building a multiligual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Print a text
+*
+*Return: by default return zero if no errors encountered
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcomeis a piece of art,\n");
+	return (0);
+}
