8cb659ed6b27bae994f4158b37fcdddd6c5153bd
GabrielParka <onwujiuba.uzochukwu@gmail.com>
2022-10-14 19:08:08 -0700

    first c program that satisfied betty

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>                                 
+
+/**
+ *main - entry point of program
+ *
+ *Return:0-no error, non zero value if error.
+ */
+int main(void)
+{
+
+}
