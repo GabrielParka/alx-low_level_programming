2e7688e04c83af4e986932e10d5619d7cd7cea25
GabrielParka <onwujiuba.uzochukwu@gmail.com>
2022-10-28 18:57:26 -0700

    fixed

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,15 +3,14 @@
  * rot13 - encodes a string using rot13
  * @s: input string.
  * Return: the pointer to dest.
- */
+**/
 
 char *rot13(char *s)
 {
 	int i, j;
 
 	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *y = "nopqrstuvwxyzabcdefghijklmNOPPQRSTUVWXYZABCDEFGHIJKLM";
-
+	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; x[j] != '\0'; j++)
@@ -24,5 +23,5 @@ char *rot13(char *s)
 		}
 	}
 
-	return (s)
+	return (s);
 }
